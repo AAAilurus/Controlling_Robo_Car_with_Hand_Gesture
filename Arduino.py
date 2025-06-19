fd4e205e80168ee0644243ec658c04a3c9c620a4
AAAilurus <158803318+AAAilurus@users.noreply.github.com>
2025-06-19 18:43:08 -0500

    Update Arduino.py

--- Arduino.py
+++ Arduino.py
@@ -1,92 +1,63 @@
-
-const int leftMotorForwardPin = 9;  // Left motor forward
-const int leftMotorBackwardPin = 8; // Left motor backward
-const int leftMotorSpeedPin = 10;   
-
-const int rightMotorForwardPin = 7;  // Right motor forward
-const int rightMotorBackwardPin = 6; // Right motor backward
-const int rightMotorSpeedPin = 5;    
-
-char command;  
+// Motor driver pin assignments
+int leftMotorInput1 = 2;   // IN1 of L298N motor driver
+int leftMotorInput2 = 3;   // IN2
+int rightMotorInput1 = 4;  // IN3
+int rightMotorInput2 = 5;  // IN4
+int leftMotorSpeedControl = 9;   // ENA (PWM for left motor)
+int rightMotorSpeedControl = 10; // ENB (PWM for right motor)
 
 void setup() {
-  pinMode(leftMotorForwardPin, OUTPUT);
-  pinMode(leftMotorBackwardPin, OUTPUT);
-  pinMode(leftMotorSpeedPin, OUTPUT);
-
-  pinMode(rightMotorForwardPin, OUTPUT);
-  pinMode(rightMotorBackwardPin, OUTPUT);
-  pinMode(rightMotorSpeedPin, OUTPUT);
-
-  Serial.begin(9600);  
+  Serial.begin(9600);  // Set up Bluetooth serial communication
+  pinMode(leftMotorInput1, OUTPUT);
+  pinMode(leftMotorInput2, OUTPUT);
+  pinMode(rightMotorInput1, OUTPUT);
+  pinMode(rightMotorInput2, OUTPUT);
+  pinMode(leftMotorSpeedControl, OUTPUT);
+  pinMode(rightMotorSpeedControl, OUTPUT);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    command = Serial.read();
-    Serial.println(command);  
+  if (Serial.available()) {
+    char receivedCommand = Serial.read();  // Read one character
 
-    if (command == 'F') {
-      moveForward();
-    } else if (command == 'B') {
-      moveBackward();
-    } else if (command == 'L') {
-      turnLeft();
-    } else if (command == 'R') {
-      turnRight();
-    } else if (command == 'S') {
-      stopCar();
+    if (receivedCommand == 'F') {
+      // Move Forward
+      digitalWrite(leftMotorInput1, HIGH);
+      digitalWrite(leftMotorInput2, LOW);
+      digitalWrite(rightMotorInput1, HIGH);
+      digitalWrite(rightMotorInput2, LOW);
     }
+    else if (receivedCommand == 'B') {
+      // Move Backward
+      digitalWrite(leftMotorInput1, LOW);
+      digitalWrite(leftMotorInput2, HIGH);
+      digitalWrite(rightMotorInput1, LOW);
+      digitalWrite(rightMotorInput2, HIGH);
+    }
+    else if (receivedCommand == 'L') {
+      // Turn Left
+      digitalWrite(leftMotorInput1, LOW);
+      digitalWrite(leftMotorInput2, HIGH);
+      digitalWrite(rightMotorInput1, HIGH);
+      digitalWrite(rightMotorInput2, LOW);
+    }
+    else if (receivedCommand == 'R') {
+      // Turn Right
+      digitalWrite(leftMotorInput1, HIGH);
+      digitalWrite(leftMotorInput2, LOW);
+      digitalWrite(rightMotorInput1, LOW);
+      digitalWrite(rightMotorInput2, HIGH);
+    }
+    else if (receivedCommand == 'S') {
+      // Stop Motors
+      digitalWrite(leftMotorInput1, LOW);
+      digitalWrite(leftMotorInput2, LOW);
+      digitalWrite(rightMotorInput1, LOW);
+      digitalWrite(rightMotorInput2, LOW);
+    }
+
+    // Set motor speed (between 0 to 255)
+    analogWrite(leftMotorSpeedControl, 150);  
+    analogWrite(rightMotorSpeedControl, 150); 
   }
-}
-
-void moveForward() {
-  digitalWrite(leftMotorForwardPin, HIGH);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  analogWrite(leftMotorSpeedPin, 200);
-
-  // Right motor forward
-  digitalWrite(rightMotorForwardPin, HIGH);
-  digitalWrite(rightMotorBackwardPin, LOW);
-  analogWrite(rightMotorSpeedPin, 200);
-}
-
-void moveBackward() {
-  digitalWrite(leftMotorForwardPin, LOW);
-  digitalWrite(leftMotorBackwardPin, HIGH);
-  analogWrite(leftMotorSpeedPin, 200);
-
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, HIGH);
-  analogWrite(rightMotorSpeedPin, 200);
-}
-
-void turnLeft() {
-  digitalWrite(leftMotorForwardPin, LOW);
-  digitalWrite(leftMotorBackwardPin, HIGH);
-  analogWrite(leftMotorSpeedPin, 180);
-
-  digitalWrite(rightMotorForwardPin, HIGH);
-  digitalWrite(rightMotorBackwardPin, LOW);
-  analogWrite(rightMotorSpeedPin, 180);
-}
-
-void turnRight() {
-  digitalWrite(leftMotorForwardPin, HIGH);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  analogWrite(leftMotorSpeedPin, 180);
-
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, HIGH);
-  analogWrite(rightMotorSpeedPin, 180);
-}
-
-void stopCar() {
-  digitalWrite(leftMotorForwardPin, LOW);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  analogWrite(leftMotorSpeedPin, 0);
-
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, LOW);
-  analogWrite(rightMotorSpeedPin, 0);
 }
